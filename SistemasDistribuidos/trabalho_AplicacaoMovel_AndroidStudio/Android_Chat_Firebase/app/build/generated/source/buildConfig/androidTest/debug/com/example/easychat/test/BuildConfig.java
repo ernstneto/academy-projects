f9ba631f9f6ec75c4f9f3e7e81f501ca674efaf6
Ernst <ernstneto94@hotmail.com>
2025-07-28 23:14:08 -0300

    adicionando SistemasDistribuidos

---
+++ SistemasDistribuidos/trabalho_AplicacaoMovel_AndroidStudio/Android_Chat_Firebase/app/build/generated/source/buildConfig/androidTest/debug/com/example/easychat/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.easychat.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.easychat.test";
+  public static final String BUILD_TYPE = "debug";
+}
